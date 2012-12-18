17e41fee0a0857842e872d4ac50f7147b84aa4aa
Arrogant Penguin <arrogantpenguin@gmail.com>
2012-12-18 07:45:19 -0500

    Added sources/texinfo

---
+++ sources/texinfo/autogen.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# $Id: autogen.sh,v 1.11 2012/02/15 09:56:50 pertusus Exp $
+# Created 2003-08-29, Karl Berry.  Public domain.
+
+if test "x$1" = x-n; then
+  chicken=true
+  echo "Ok, playing chicken; not actually running any commands."
+else
+  chicken=
+fi
+
+echo "Preparing Texinfo development infrastructure:"
+
+# Generates an include file for tp/Makefile.am.
+cmd="./tp/maintain/regenerate_file_lists.pl"
+echo "  $cmd"
+$chicken eval $cmd || exit 1
+
+# Generates another include file for tp/Makefile.am.
+cmd="./tp/maintain/regenerate_document_strings_make_rules.sh"
+echo "  $cmd"
+$chicken eval $cmd || exit 1
+
+# This overwrites lots of files with older versions.
+# I keep the newest versions of files common between distributions up to
+# date in CVS (see util/srclist.txt), because it's not trivial for every
+# developer to do this.
+#cmd="autoreconf --verbose --force --install --include=m4"
+
+# So instead:
+: ${ACLOCAL=aclocal}
+: ${AUTOHEADER=autoheader}
+: ${AUTOMAKE=automake}
+: ${AUTOCONF=autoconf}
+cmd="$ACLOCAL -I gnulib/m4 && $AUTOCONF && $AUTOHEADER && $AUTOMAKE"
+echo "  $cmd $*"
+$chicken eval $cmd "$@" || exit 1
+
+echo
+echo "Now run configure with your desired options, for instance:"
+echo "  ./configure CFLAGS='-g'"
---
+++ sources/texinfo/texi2html/test/manage_test_archives.sh
@@ -0,0 +1,74 @@
+#! /bin/sh
+
+user=pertusus
+
+command=$1
+
+if [ "z$command" = 'z' ]; then
+  echo "Need something to do (pack/packt2h/unpack/get/clean/upload)"
+  exit 1
+fi
+shift;
+
+if [ z"$1" != 'z' ]; then
+  user=$1
+fi
+
+download_dir="download-mirror.savannah.gnu.org/releases/texinfo"
+
+VERSION=`grep '^VERSION = ' ../../Makefile | sed 's/^VERSION = *//'`
+if [ z"$VERSION" = 'z' ]; then
+  echo "Cannot find version"
+  exit 1
+fi
+
+tp_tests_name=tp_tests_results-$VERSION
+texi2html_tests_name=t2h_tests_results
+#manuals=t2h_tests_big_manuals
+
+if [ $command = 'packt2h' ]; then
+  rm -f $texi2html_tests_name.tar.gz #$manuals.tar.gz
+  (
+  cd ..
+  tar c --exclude-vcs -z -f test/$texi2html_tests_name.tar.gz test/*/res/ test/*/res_all/ test/*/res_info/ test/*/res_html/ test/*/res_docbook/ test/*/res_xml/ test/many_input_files/*_res/
+  #tar c --exclude-vcs -z -f test/$manuals.tar.gz test/manuals/*.texi test/tar_manual/*.texi test/singular_manual/*.tex* test/singular_manual/d2t_singular/
+  )
+elif [ $command = 'pack' ]; then
+  rm -f $tp_tests_name.tar.gz
+  (
+  cd ..
+  tar c --exclude-vcs -z -f test/$tp_tests_name.tar.gz test/*/res_parser*/
+  )
+elif [ $command = 'get' ]; then :
+  wget -N -r -np -A '*.tar.gz*' http://$download_dir/ || exit 1
+  for file in $texi2html_tests_name $tp_tests_name; do
+    if [ -f  $download_dir/$file.tar.gz ]; then
+      cp -a $download_dir/$file.tar.gz ..
+    fi
+  done
+elif [ $command = 'clean' ]; then
+  rm -rf download-mirror.savannah.gnu.org
+  rm -f $texi2html_tests_name.tar.gz $tp_tests_name.tar.gz
+  rm -f ../$texi2html_tests_name.tar.gz ../$tp_tests_name.tar.gz
+elif [ $command = 'unpack' ]; then
+  (
+  cd ..
+  for file in $texi2html_tests_name $tp_tests_name; do
+    if [ -f $file.tar.gz ]; then
+      tar x -z -f $file.tar.gz
+    fi
+  done
+  #tar x -z -f test/$manuals.tar.gz
+  )
+elif [ $command = 'upload' ]; then
+  mkdir -p ../upload
+  #cp -a $texi2html_tests_name.tar.gz upload
+  #gpg -b --use-agent upload/$texi2html_tests_name.tar.gz
+  cp -a $tp_tests_name.tar.gz ../upload
+  gpg -b --use-agent ../upload/$tp_tests_name.tar.gz
+  rsync -a -essh ../upload/ $user@dl.sv.gnu.org:/releases/texinfo/
+else
+  echo "Unknown command (pack/packt2h/unpack/get/clean/upload)"
+  exit 1
+fi
+
